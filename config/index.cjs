79885435d3dd3505434bf560f8f1f51bcaa84097
DearYYY <99167760+DearYYY@users.noreply.github.com>
2023-02-15 17:40:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '琳琳大小姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oL_N-6bZEh1pqYAW7dewzmHCq8XU',
+      id: 'oL_N-6bZEh1pqYAW7dewzmHCq8XU','oL_N-6e39vdE8TZsaYiBVP8g76bU'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2Yf8PYlD2mMQXFH-q6AqDW7yQSUTKavTn8VfvUGa4H8',
+      useTemplateId: 'tqcJlNida-ncC0tuh5Q_dQyflIkRLUGKGWOTrxhtWYw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
      horoscopeDate: '02-19',
       festivals: [
